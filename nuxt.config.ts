c182b72f10331f2e58513f379e73faae75a4e0e9
Federico Mameli <federico_mameli@yahoo.com>
2025-02-01 17:58:04 +0000

    feat: create nuxt app with nuxt ui v3 + firebase

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui',
+    'nuxt-firebase-emulators',
+    'nuxt-vuefire',
+    '@nuxt/eslint'
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: '2024-11-27'
+})
