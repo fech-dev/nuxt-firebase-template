43c5fa45fbe5d40f50c4431994fc6f0be1f75636
Federico Mameli <federico_mameli@yahoo.com>
2025-02-03 15:04:05 +0000

    chore: add vuefire config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,5 +15,19 @@ export default defineNuxtConfig({
     compatibilityVersion: 4
   },
 
-  compatibilityDate: '2024-11-27'
+  compatibilityDate: '2024-11-27',
+
+  vuefire: {
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+    },
+    auth: {
+      enabled: true,
+    },
+  },
 })
