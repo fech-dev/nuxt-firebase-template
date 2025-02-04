9a65c88fa16ecd6f183cf982f3091e54affcbd78
Federico Mameli <federico_mameli@yahoo.com>
2025-02-04 22:10:00 +0000

    chore(devcontainer): update post-create script to create .env file

--- .devcontainer/commands/post-create.sh
+++ .devcontainer/commands/post-create.sh
@@ -2,4 +2,8 @@
 echo "⚙️ Setting up pnpm store..."
 pnpm config set store-dir /home/node/.pnpm-store 
 sudo chown -R node:node /home/node/.pnpm-store
-echo "✅ Done!.
+echo "✅ Done!"
+
+# create .env file
+cp .env.example .env 
+echo "✅ .env file created!"
